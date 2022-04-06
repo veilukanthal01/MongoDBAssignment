ae0fe5eaebfa8d5c52215024ba8ecf4a247a11e4
veilukanthal01 <88130501+veilukanthal01@users.noreply.github.com>
2022-04-06 20:35:37 +0530

    added comments

--- Driver.java
+++ Driver.java
@@ -19,6 +19,9 @@ import java.util.List;
 
 public class Driver {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        /**
+         * Creating a MongoClient , Database and Collection
+         */
         MongoClient mongoClient = MongoClients.create("mongodb://ec2-54-173-40-93.compute-1.amazonaws.com");
         if (mongoClient != null)
             System.out.println("Connected to the Mongo DB" + mongoClient);
@@ -27,6 +30,10 @@ public class Driver {
         //db.getCollection("products").drop();
         MongoCollection<Document> productsCollection = db.getCollection("products");
         System.out.println("Collection Created " + productsCollection);
+
+        /**
+         * Importing Data from MYSQL to Mongodb and displaying all the documents
+         */
         Driver driver = new Driver();
         //driver.importDataToMongoDB(productsCollection);
         for (Document document : productsCollection.find()) {
@@ -36,6 +43,9 @@ public class Driver {
     }
 
     public void importDataToMongoDB(MongoCollection<Document> productsCollection) throws SQLException, ClassNotFoundException {
+        /**
+         * Importing mobiles, cameras and headphones data to MongoDB
+         */
         LoadMobileTabletoProductCollection(productsCollection, "Mobiles");
         LoadMobileTabletoProductCollection(productsCollection, "Cameras");
         LoadMobileTabletoProductCollection(productsCollection, "Headphones");
@@ -46,14 +56,14 @@ public class Driver {
         String url = "jdbc:mysql://pgc-sd-bigdata.cyaielc9bmnf.us-east-1.rds.amazonaws.com:3306/pgcdata";
         String user = "student";
         String password = "STUDENT123";
-        List<Document> mobilesList = new ArrayList<Document>();
+        List<Document> productsList = new ArrayList<Document>();
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = null;
         Statement statement = null;
         String sql = null;
         try {
             /**
-             * Creating a connection with MYSQL
+             * Creating a connection to MYSQL
              */
             connection = DriverManager.getConnection(url, user, password);
 
@@ -64,6 +74,9 @@ public class Driver {
                 System.out.println("Connected to the MYSQL database");
             }
 
+            /**
+             * Creating sql query to be executed based on the Category name
+             */
             if (categoryName.equalsIgnoreCase("Mobiles"))
                 sql = "select * from mobiles";
             else if (categoryName.equalsIgnoreCase("Cameras"))
@@ -71,11 +84,21 @@ public class Driver {
             else
                 sql = "select * from headphones";
 
+            /**
+             * Retrieving result sets from mysql
+             */
             statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
+
+            /**
+             * Getting  Column count using metadata interface
+             */
             ResultSetMetaData metadata = resultSet.getMetaData();
             int columnCount = metadata.getColumnCount();
 
+            /**
+             * Traversing the result set and creating documents to be imported to MongoDB
+             */
             while (resultSet.next()) {
                 Document document = new Document("ProductId", resultSet.getString("ProductId"))
                         .append("CategoryName", categoryName);
@@ -83,9 +106,12 @@ public class Driver {
                     String columnName = metadata.getColumnName(i);
                     document.append(columnName, resultSet.getString(columnName));
                 }
-                mobilesList.add(document);
+                productsList.add(document);
             }
-            productsCollection.insertMany(mobilesList);
+            /**
+             * Inserting created documents into mongodb
+             */
+            productsCollection.insertMany(productsList);
 
 
         } finally {
